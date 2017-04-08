71dc7c2ba195a54236b7e6b4360306b4ad52a670
Beth Cummings <bethios@gmail.com>
2017-04-08 15:16:50 -0500

    add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -105,12 +105,18 @@ RSpec.describe User, type: :model do
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
     let(:user_with_invalid_email) { User.new(name: "bloccit User", email: "") }
+    let(:user_with_invalid_both) { User.new(name: "", email: "") }
+
 
     it "should be an invalid user due to blank name" do
       expect(user_with_invalid_name).to_not be_valid
     end
 
     it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+
+    it "should be an invalid user due to blank email and name" do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
